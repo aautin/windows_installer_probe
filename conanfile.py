46f5db0a0281251e2dd2620754505325b95f5680
Autin Alexandre <alexandre.autin@sbg-systems.com>
2025-11-10 14:49:43 +0100

    Add @SBG-Systems qt-binaries aside of conan-center package

--- conanfile.py
+++ conanfile.py
@@ -1,3 +1,4 @@
+import os
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 
@@ -5,12 +6,18 @@ class WindowsInstallerProbe(ConanFile):
 	settings = 'os', 'compiler', 'build_type', 'arch'
 	
 	def requirements(self):
-		self.requires('qt/6.7.3')
+		if os.getenv('COMPUTERNAME', '') == 'FR-L-F0DW424':
+			self.requires('qt-binaries/6.7.3')
+			self.is_binaries = 'qt-binaries'
+		else:
+			self.requires('qt/6.7.3')
+			self.qt_package = 'qt'
 	
 	def generate(self):
 		deps = CMakeDeps(self)
 		deps.generate()
 		tc = CMakeToolchain(self)
+		tc.variables["QT_PACKAGE_NAME"] = self.qt_package
 		tc.generate()
  
 	def layout(self):
